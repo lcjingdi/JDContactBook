21184f37a42b8944a7c7fd06f84a1c0717d72bd0
lcjingdi <lcjingdi@gmail.com>
2017-08-11 15:20:27 +0800

    Initial Commit

---
+++ JDContactBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JDContactBook
+//
+//  Created by jingdi on 2017/8/11.
+//  Copyright © 2017年 lcjingdi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JDContactBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JDContactBook
+//
+//  Created by jingdi on 2017/8/11.
+//  Copyright © 2017年 lcjingdi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
